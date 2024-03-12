13014e9994ba5d2eb54faa284663a62391a89022
yeni <ngeetl@naver.com>
2024-03-12 15:03:19 +0900

    메모앱 기초 셋팅

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="header">
+      <div class="bg-gray-50 flex justify-center items-center w-full py-10 text-3xl font-thin">My Memo</div>
+    </div>
+  </template>
+  
+  <script>
+  
+  </script>
+  
+  
---
+++ src/components/Memo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="memo">
+      <ul>
+        <li v-for="(item, idx) in data" :key="idx">{{ item }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    setup() {
+        const data = [
+            "메모 1",
+            "메모 2",
+            "메모 3",
+        ]
+
+        return { data };
+    }    
+  }
+  </script>
+  
+  <style scoped>
+  .memo ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  </style>
+  
